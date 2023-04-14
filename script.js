95495d84cf05a5fd479fb322324a634715f0dca1
srimathan <76614499+srimathans@users.noreply.github.com>
2023-04-14 20:35:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const menuItems = document.querySelector(".menu-items");
-
-hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  menuItems.classList.toggle("active");
-});
