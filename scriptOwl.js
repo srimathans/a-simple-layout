457709f6bbe4cd3a242ad2f8099cc80f0d867bca
srimathan <76614499+srimathans@users.noreply.github.com>
2023-04-15 13:28:38 +0530

    Add files via upload

---
+++ scriptOwl.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
